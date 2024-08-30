e84f92cd9c2af06a45f142f42d5802b0400aee00
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-08-30 23:53:29 +0530

    Added All Functionality of Web Socket Handlers And Route To Receive payload and userId for sendng it to the frontend clients

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ const express = require("express");
 const { Server } = require("socket.io");
 const http = require("http");
 const cors = require("cors");
+const Redis = require("ioredis");
+
+const redisCache = new Redis();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -18,8 +25,40 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("New User Connected", socket.id);
 
-  socket.on("disconnect", () => {
+  socket.on("setUserId", async (userId) => {
+    await redisCache.set(userId, socket.id);
+    console.log(
+      "Successfully Mapped User Id: " + userId + " To Socket: " + socket.id
+    );
+  });
+
+  socket.on("disconnect", async () => {
     console.log("User Disconnected", socket.id);
+  });
+});
+
+app.post("/sendPayload", async (req, res) => {
+  const { userId, payload } = req.body;
+  if (!userId || !payload) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Request",
+    });
+  }
+
+  const socketId = await redisCache.get(userId);
+  if (!socketId) {
+    return res.status(404).json({
+      success: false,
+      message: "Connection Not Found",
+    });
+  }
+
+  io.to(socketId).emit("submissionPayloadResponse", payload);
+
+  return res.status(200).json({
+    success: true,
+    message: "Payload Sent To User Successfully",
   });
 });
 
