a629192ee95471bc516572bbfefc0cdb35789e12
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-09-12 23:56:16 +0530

    better logging system using console

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const Redis = require("ioredis");
 
 const redisCache = new Redis();
+console.log("Redis cache initialized.");
 
 const app = express();
 app.use(cors());
@@ -23,23 +24,29 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("New User Connected", socket.id);
+  console.log("New User Connected:", socket.id);
 
   socket.on("setUserId", async (userId) => {
     await redisCache.set(userId, socket.id);
     console.log(
-      "Successfully Mapped User Id: " + userId + " To Socket: " + socket.id
+      "Successfully Mapped User Id:",
+      userId,
+      "To Socket:",
+      socket.id
     );
   });
 
   socket.on("disconnect", async () => {
-    console.log("User Disconnected", socket.id);
+    console.log("User Disconnected:", socket.id);
   });
 });
 
 app.post("/sendPayload", async (req, res) => {
   const { userId, payload } = req.body;
+  console.log("Received payload request for userId:", userId);
+
   if (!userId || !payload) {
+    console.log("Invalid request received.");
     return res.status(400).json({
       success: false,
       message: "Invalid Request",
@@ -48,6 +55,7 @@ app.post("/sendPayload", async (req, res) => {
 
   const socketId = await redisCache.get(userId);
   if (!socketId) {
+    console.log("Connection not found for userId:", userId);
     return res.status(404).json({
       success: false,
       message: "Connection Not Found",
@@ -55,6 +63,7 @@ app.post("/sendPayload", async (req, res) => {
   }
 
   io.to(socketId).emit("submissionPayloadResponse", payload);
+  console.log("Payload sent to userId:", userId);
 
   return res.status(200).json({
     success: true,
